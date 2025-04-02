49e080eacc39831a96e6adbd36e7dff98a0f951f
seongmin-wxnr <yuilove9105@gmail.com>
2025-04-02 20:51:51 +0900

    사각형 넓이 구하는 문제

---
+++ b15894_RecSize.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/// <summary>
+/// 뭔가 줫같은 문제
+/// </summary>
+ 
+
+int main() {
+	unsigned long size_v; 
+	std::cin >> size_v;
+	std::cout << 4 * size_v << std::endl;
+}
